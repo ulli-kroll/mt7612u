91c924f871e0ea5045427cb2b7c1b65b42ae0430
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2017-02-12 19:20:08 +0100

    refractoring RTMP_IO_FORCE_WRITE32() macro
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- include/os/rt_linux.h
+++ include/os/rt_linux.h
@@ -692,11 +692,8 @@ void linux_pci_unmap_single(void *handle, ra_dma_addr_t dma_addr, size_t size, i
 #define RTMP_IO_FORCE_READ32(_A, _R, _pV)								\
 	RTUSBReadMACRegister((_A), (_R), (uint32_t *) (_pV))
 
-#define RTMP_IO_FORCE_WRITE32(_A, _R, _V)	\
-	do{\
-		/*if ((_R) != 0x404)*/ /* TODO:shiang-6590, depends on sw porting guide, don't acccess it now */\
-			RTUSBWriteMACRegister((_A), (_R), (uint32_t) (_V));		\
-	}while(0)
+#define RTMP_IO_FORCE_WRITE32(_A, _R, _V) \
+	RTUSBWriteMACRegister((_A), (_R), (uint32_t) (_V));
 
 #define RTMP_IO_READ32(_A, _R, _pV)								\
 	RTUSBReadMACRegister((_A), (_R), (uint32_t *) (_pV))
