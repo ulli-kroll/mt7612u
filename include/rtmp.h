6e60cb6a41e2597b4398d318d5f63316b978c0a0
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2017-02-25 11:53:56 +0100

    remove unused Set_ITxBfEn_Proc()
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- include/rtmp.h
+++ include/rtmp.h
@@ -6783,7 +6783,6 @@ INT	Set_StaETxBfEnCond_Proc(struct rtmp_adapter *pAd, char *arg);
 INT	Set_NoSndgCntThrd_Proc(struct rtmp_adapter *pAd, char *arg);
 INT	Set_NdpSndgStreams_Proc(struct rtmp_adapter *pAd, char *arg);
 INT	Set_Trigger_Sounding_Proc(struct rtmp_adapter *pAd, char *arg);
-INT	Set_ITxBfEn_Proc(struct rtmp_adapter *pAd, char *arg);
 
 #ifdef MT76x2
 INT Set_TxBfProfileTag_Help(
