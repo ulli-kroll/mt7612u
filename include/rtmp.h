8ceb83989fadfae491426c41166bd979233e1836
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2017-02-25 12:17:50 +0100

    remove unused Set_TxBfProfileTagRead()
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- include/rtmp.h
+++ include/rtmp.h
@@ -6785,10 +6785,6 @@ INT	Set_NdpSndgStreams_Proc(struct rtmp_adapter *pAd, char *arg);
 INT	Set_Trigger_Sounding_Proc(struct rtmp_adapter *pAd, char *arg);
 
 #ifdef MT76x2
-INT Set_TxBfProfileTagRead(
-    IN  struct rtmp_adapter *  pAd,
-	IN  char *        arg);
-
 INT Set_TxBfProfileTagWrite(
     IN  struct rtmp_adapter *  pAd,
 	IN  char *        arg);
