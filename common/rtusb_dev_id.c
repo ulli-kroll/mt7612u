bfb23a507eee98951de65421cf695b4247468755
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2017-02-25 17:21:47 +0100

    add MS XBOX ONE wireless adapter USB-ID
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- common/rtusb_dev_id.c
+++ common/rtusb_dev_id.c
@@ -41,6 +41,7 @@ USB_DEVICE_ID rtusb_dev_id[] = {
 	{ USB_DEVICE(0x7392, 0xb711) },	/*  Edimax EW 7722 UAC */
 	{ USB_DEVICE(0x0846, 0x9053) },	/*  Netgear A6210 */
 	{ USB_DEVICE(0x057c, 0x8503) },	/*  AVM FRITZ!WLAN USB Stick AC 860 */
+	{ USB_DEVICE(0x045e, 0x02e6) },	/*  Microsoft XBox One Wireless Adapter */
 #endif
 	{ }/* Terminating entry */
 };
