0568de6a1e3c865823ccb705021a705e32de6fbd
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2017-03-23 04:10:31 +0100

    remove unused Set_StaETxBfEnCond_Proc(), for txbf
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- include/rtmp.h
+++ include/rtmp.h
@@ -6407,7 +6407,6 @@ INT	Set_ITxBfLnaCal_Proc(struct rtmp_adapter *pAd, char *arg);
 INT	Set_ETxBfEnCond_Proc(struct rtmp_adapter *pAd, char *arg);
 INT Set_ETxBfNoncompress_Proc(struct rtmp_adapter *pAd, char *arg);
 INT Set_ETxBfIncapable_Proc(struct rtmp_adapter *pAd, char *arg);
-INT	Set_StaETxBfEnCond_Proc(struct rtmp_adapter *pAd, char *arg);
 INT	Set_NoSndgCntThrd_Proc(struct rtmp_adapter *pAd, char *arg);
 INT	Set_NdpSndgStreams_Proc(struct rtmp_adapter *pAd, char *arg);
 INT	Set_Trigger_Sounding_Proc(struct rtmp_adapter *pAd, char *arg);
