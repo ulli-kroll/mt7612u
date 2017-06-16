16e4a5efb32d476d7fb52a915da2790948722ed5
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2017-06-16 12:42:44 +0200

    small cleanup
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- mac/rtmp_mac.c
+++ mac/rtmp_mac.c
@@ -107,20 +107,16 @@ VOID RTMPWriteTxWI(
 	else if (MT_REV_ET(pAd, MT76x2, REV_MT76x2E3))
 		tx_stream_mode = (pTransmit->field.MODE <= MODE_OFDM) ? 0x93 : 0x0;
 
-	if (pMac)
-	{
-		if (pAd->CommonCfg.bMIMOPSEnable)
-		{
-			if ((pMac->MmpsMode == MMPS_DYNAMIC) && (pTransmit->field.MCS > 7))
-			{
+	if (pMac) {
+		if (pAd->CommonCfg.bMIMOPSEnable) {
+			if ((pMac->MmpsMode == MMPS_DYNAMIC) &&
+			    (pTransmit->field.MCS > 7)) {
 				/* Dynamic MIMO Power Save Mode*/
 				mimops = 1;
-			}
-			else if (pMac->MmpsMode == MMPS_STATIC)
-			{
+			} else if (pMac->MmpsMode == MMPS_STATIC) {
 				/* Static MIMO Power Save Mode*/
-				if (pTransmit->field.MODE >= MODE_HTMIX && pTransmit->field.MCS > 7)
-				{
+				if (pTransmit->field.MODE >= MODE_HTMIX &&
+				    pTransmit->field.MCS > 7) {
 					mcs = 7;
 					mimops = 0;
 				}
@@ -138,10 +134,9 @@ VOID RTMPWriteTxWI(
 	TxEAPId_Cal = WCID + (UINT)((pAd->CommonCfg.Bssid[5] >> 4) ^ (pAd->CommonCfg.Bssid[5] & 0x0F))*32;
 	TxEAPId_Cal -= ((TxEAPId_Cal >> 9) << 9);
 
-	if (pMac && pAd->chipCap.FlgHwTxBfCap)
-	{
-		if (pMac->TxSndgType == SNDG_TYPE_NDP  || pMac->TxSndgType == SNDG_TYPE_SOUNDING)
-		{
+	if (pMac && pAd->chipCap.FlgHwTxBfCap) {
+		if (pMac->TxSndgType == SNDG_TYPE_NDP  ||
+		    pMac->TxSndgType == SNDG_TYPE_SOUNDING) {
 			stbc  = false;
 			eTxBf = false;
 			iTxBf = false;
@@ -155,7 +150,8 @@ VOID RTMPWriteTxWI(
 		}
 	}
 
-	if (iTxBf || eTxBf) stbc = 0; // Force STBC = 0 when TxBf is enabled
+	if (iTxBf || eTxBf)
+		stbc = 0; // Force STBC = 0 when TxBf is enabled
 
 
 
@@ -261,20 +257,18 @@ VOID RTMPWriteTxWI_Data(struct rtmp_adapter *pAd, TXWI_STRUC *pTxWI, TX_BLK *pTx
 
 	ampdu = ((pTxBlk->TxFrameType == TX_AMPDU_FRAME) ? true : false);
 	basize = pAd->CommonCfg.TxBASize;
-	if(ampdu && pMacEntry)
-	{
+	if(ampdu && pMacEntry) {
 		/*
  		 * Under HT20, 2x2 chipset, OPEN, and with some atero chipsets
  		 * reduce BASize to 7 to add one bulk A-MPDU during one TXOP
  		 * to improve throughput
  		 */
-		if ((pAd->CommonCfg.BBPCurrentBW == BW_20) && (pAd->Antenna.field.TxPath == 2)
-			&& (pMacEntry->bIAmBadAtheros) && (pMacEntry->WepStatus == Ndis802_11EncryptionDisabled))
-		{
+		if ((pAd->CommonCfg.BBPCurrentBW == BW_20) &&
+		    (pAd->Antenna.field.TxPath == 2) &&
+		    (pMacEntry->bIAmBadAtheros) &&
+		    (pMacEntry->WepStatus == Ndis802_11EncryptionDisabled)) {
 			basize = 7;
-		}
-		else
-		{
+		} else {
 			u8 RABAOriIdx = pTxBlk->pMacEntry->BAOriWcidArray[pTxBlk->UserPriority];
 			basize = pAd->BATable.BAOriEntry[RABAOriIdx].BAWinSize;
 		}
@@ -284,8 +278,7 @@ VOID RTMPWriteTxWI_Data(struct rtmp_adapter *pAd, TXWI_STRUC *pTxWI, TX_BLK *pTx
 		ampdu = false;
 
 	sounding = ndp_bw = ndp_rate = iTxBf = eTxBf = 0;
-	if (pTxBlk->TxSndgPkt == SNDG_TYPE_SOUNDING)
-	{
+	if (pTxBlk->TxSndgPkt == SNDG_TYPE_SOUNDING) {
 		sounding = 1;
 		iTxBf = false;
 		eTxBf = false;
@@ -293,9 +286,7 @@ VOID RTMPWriteTxWI_Data(struct rtmp_adapter *pAd, TXWI_STRUC *pTxWI, TX_BLK *pTx
 		DBGPRINT(RT_DEBUG_TRACE, ("ETxBF in %s(): sending normal sounding, eTxBF=%d\n",
 					__FUNCTION__, pTransmit->field.eTxBF));
 		iTxBf = 0;
-	}
-	else if (pTxBlk->TxSndgPkt == SNDG_TYPE_NDP)
-	{
+	} else if (pTxBlk->TxSndgPkt == SNDG_TYPE_NDP) {
 		ndp_bw = pTransmit->field.BW;
 		iTxBf = false;
 		eTxBf = false;
@@ -309,16 +300,13 @@ VOID RTMPWriteTxWI_Data(struct rtmp_adapter *pAd, TXWI_STRUC *pTxWI, TX_BLK *pTx
 
 		DBGPRINT(RT_DEBUG_TRACE, ("ETxBF in %s(): NDP sounding, eTxBF=%d, ndp_rate = %d\n",
 					__FUNCTION__, eTxBf, ndp_rate));
-	}
-	else
-	{
-			eTxBf = pTransmit->field.eTxBF;
+	} else {
+		eTxBf = pTransmit->field.eTxBF;
 		iTxBf = pTransmit->field.iTxBF;
 	}
 
-	if (iTxBf || eTxBf) stbc = false; // Force STBC = false when TxBf is enabled
-
-
+	if (iTxBf || eTxBf)
+		stbc = false; // Force STBC = false when TxBf is enabled
 
 	if (pTxBlk->TxSndgPkt > SNDG_TYPE_DISABLE)
 	{
@@ -347,8 +335,7 @@ VOID RTMPWriteTxWI_Data(struct rtmp_adapter *pAd, TXWI_STRUC *pTxWI, TX_BLK *pTx
 	}
 
 #ifdef DBG_DIAGNOSE
-	if (pTxBlk->QueIdx== 0)
-	{
+	if (pTxBlk->QueIdx== 0) {
 		pAd->DiagStruct.diag_info[pAd->DiagStruct.ArrayCurIdx].TxDataCnt++;
 #ifdef DBG_TX_MCS
 		pAd->DiagStruct.diag_info[pAd->DiagStruct.ArrayCurIdx].TxMcsCnt_HT[mcs]++;
@@ -362,8 +349,7 @@ VOID RTMPWriteTxWI_Data(struct rtmp_adapter *pAd, TXWI_STRUC *pTxWI, TX_BLK *pTx
 #ifdef MCS_LUT_SUPPORT
 	if ((RTMP_TEST_MORE_FLAG(pAd, fASIC_CAP_MCS_LUT)) &&
 		(wcid < 128) &&
-		(pMacEntry && pMacEntry->bAutoTxRateSwitch == true))
-	{
+		(pMacEntry && pMacEntry->bAutoTxRateSwitch == true)) {
 		HTTRANSMIT_SETTING rate_ctrl;
 
 		rate_ctrl.field.MODE = phy_mode;
@@ -379,8 +365,9 @@ VOID RTMPWriteTxWI_Data(struct rtmp_adapter *pAd, TXWI_STRUC *pTxWI, TX_BLK *pTx
 	}
 #ifdef PEER_DELBA_TX_ADAPT
 	GET_GroupKey_WCID(pAd, mbc_wcid, pTxBlk->apidx);
-	if (RTMP_GET_PACKET_LOWRATE(pTxBlk->pPacket) || (wcid == mbc_wcid) ||
-		(pMacEntry && (pMacEntry->MmpsMode == MMPS_STATIC)))
+	if (RTMP_GET_PACKET_LOWRATE(pTxBlk->pPacket) ||
+	   (wcid == mbc_wcid) ||
+	   (pMacEntry && (pMacEntry->MmpsMode == MMPS_STATIC)))
 		lut_enable = false;
 	else
 		lut_enable = true;
@@ -388,9 +375,9 @@ VOID RTMPWriteTxWI_Data(struct rtmp_adapter *pAd, TXWI_STRUC *pTxWI, TX_BLK *pTx
 #endif /* MCS_LUT_SUPPORT */
 
 #ifdef MCS_LUT_SUPPORT
-	if (pMacEntry && pAd->chipCap.FlgHwTxBfCap)
-	{
-		if (pTxBlk->TxSndgPkt == SNDG_TYPE_NDP  || pTxBlk->TxSndgPkt == SNDG_TYPE_SOUNDING)
+	if (pMacEntry && pAd->chipCap.FlgHwTxBfCap) {
+		if (pTxBlk->TxSndgPkt == SNDG_TYPE_NDP  ||
+		    pTxBlk->TxSndgPkt == SNDG_TYPE_SOUNDING)
 			lut_enable = false;
 	}
 #endif /* MCS_LUT_SUPPORT */
@@ -400,13 +387,10 @@ VOID RTMPWriteTxWI_Data(struct rtmp_adapter *pAd, TXWI_STRUC *pTxWI, TX_BLK *pTx
 
 		txwi_n->FRAG = TX_BLK_TEST_FLAG(pTxBlk, fTX_bAllowFrag);
 		txwi_n->ACK = TX_BLK_TEST_FLAG(pTxBlk, fTX_bAckRequired);
-		if (RTMP_GET_PACKET_TDLS_WAIT_ACK(pTxBlk->pPacket))
-		{
+		if (RTMP_GET_PACKET_TDLS_WAIT_ACK(pTxBlk->pPacket)) {
 			txwi_n->TxPktId |= 0x80;
 			DBGPRINT(RT_DEBUG_INFO,("PktID |= 0x80 : [%x]\n",txwi_n->TxPktId));
-		}
-		else
-		{
+		} else {
 			txwi_n->TxPktId &= 0x7f;
 			DBGPRINT(RT_DEBUG_INFO,("PktID : [%x]\n",txwi_n->TxPktId));
 		}
@@ -486,37 +470,35 @@ VOID RTMPWriteTxWI_Cache(struct rtmp_adapter *pAd, TXWI_STRUC *pTxWI, TX_BLK *pT
 		ampdu = false;
 
 	if (bw)
-		bw = (pAd->CommonCfg.AddHTInfo.AddHtInfo.RecomWidth == 0) ? (BW_20) : (pTransmit->field.BW);
+		bw = (pAd->CommonCfg.AddHTInfo.AddHtInfo.RecomWidth == 0) ?
+			(BW_20) : (pTransmit->field.BW);
 
 	mimops = 0;
-	if (pAd->CommonCfg.bMIMOPSEnable)
-	{
+	if (pAd->CommonCfg.bMIMOPSEnable) {
 		/* MIMO Power Save Mode*/
-		if ((pMacEntry->MmpsMode == MMPS_DYNAMIC) && (pTransmit->field.MCS > 7))
+		if ((pMacEntry->MmpsMode == MMPS_DYNAMIC) &&
+		    (pTransmit->field.MCS > 7))
 			mimops = 1;
-		else if (pMacEntry->MmpsMode == MMPS_STATIC)
-		{
-			if ((pTransmit->field.MODE >= MODE_HTMIX) && (pTransmit->field.MCS > 7))
-			{
+		else if (pMacEntry->MmpsMode == MMPS_STATIC) {
+			if ((pTransmit->field.MODE >= MODE_HTMIX) &&
+			    (pTransmit->field.MCS > 7)) {
 				mcs = 7;
 				mimops = 0;
 			}
 		}
 	}
 
-	if(ampdu && pMacEntry)
-	{
+	if(ampdu && pMacEntry) {
 		/*
  		 * Under HT20, 2x2 chipset, OPEN, and with some atero chipsets
  		 * reduce BASize to 7 to add one bulk A-MPDU during one TXOP
  		 * to improve throughput
  		 */
-		if ((pAd->CommonCfg.BBPCurrentBW == BW_20) && (pMacEntry->bIAmBadAtheros))
-		{
+		if ((pAd->CommonCfg.BBPCurrentBW == BW_20) &&
+		    (pMacEntry->bIAmBadAtheros)) {
 			mpdu_density = 7;
 			if ((pAd->Antenna.field.TxPath == 2) &&
-				(pMacEntry->WepStatus == Ndis802_11EncryptionDisabled))
-			{
+			    (pMacEntry->WepStatus == Ndis802_11EncryptionDisabled)) {
 				basize = 7;
 			}
 		}
@@ -524,8 +506,7 @@ VOID RTMPWriteTxWI_Cache(struct rtmp_adapter *pAd, TXWI_STRUC *pTxWI, TX_BLK *pT
 	}
 
 #ifdef DBG_DIAGNOSE
-	if (pTxBlk->QueIdx== 0)
-	{
+	if (pTxBlk->QueIdx== 0) {
 		pAd->DiagStruct.diag_info[pAd->DiagStruct.ArrayCurIdx].TxDataCnt++;
 #ifdef DBG_TX_MCS
 		pAd->DiagStruct.diag_info[pAd->DiagStruct.ArrayCurIdx].TxMcsCnt_HT[mcs]++;
@@ -534,15 +515,12 @@ VOID RTMPWriteTxWI_Cache(struct rtmp_adapter *pAd, TXWI_STRUC *pTxWI, TX_BLK *pT
 #endif /* DBG_DIAGNOSE */
 
 	sounding = eTxBf = iTxBf = ndp_bw = ndp_rate = 0;
-	if (pTxBlk->TxSndgPkt == SNDG_TYPE_SOUNDING)
-	{
+	if (pTxBlk->TxSndgPkt == SNDG_TYPE_SOUNDING) {
 		sounding = 1;
 		stbc = false;
 		DBGPRINT(RT_DEBUG_TRACE, ("ETxBF in %s(): sending normal sounding, eTxBF=%d\n",
 					__FUNCTION__, pTransmit->field.eTxBF));
-	}
-	else if (pTxBlk->TxSndgPkt == SNDG_TYPE_NDP)
-	{
+	} else if (pTxBlk->TxSndgPkt == SNDG_TYPE_NDP) {
 		stbc = false;
 		if (pTxBlk->TxNDPSndgMcs>=16)
 			ndp_rate = 2;
@@ -554,18 +532,16 @@ VOID RTMPWriteTxWI_Cache(struct rtmp_adapter *pAd, TXWI_STRUC *pTxWI, TX_BLK *pT
 
 		DBGPRINT(RT_DEBUG_TRACE, ("ETxBF in %s(): NDP sounding, eTxBF=%d, ndp_rate = %d\n",
 					__FUNCTION__, eTxBf, ndp_rate));
-	}
-	else
-	{
+	} else {
 			eTxBf = pTransmit->field.eTxBF;
 
 		iTxBf = pTransmit->field.iTxBF;
 	}
 
-	if (eTxBf || iTxBf) stbc = false; //Force STBC = false when TxBf is enabled
+	if (eTxBf || iTxBf)
+		stbc = false; //Force STBC = false when TxBf is enabled
 
-	if (pTxBlk->TxSndgPkt > SNDG_TYPE_DISABLE)
-	{
+	if (pTxBlk->TxSndgPkt > SNDG_TYPE_DISABLE) {
 		mcs = 0;
 		ampdu = false;
 	}
@@ -575,9 +551,8 @@ VOID RTMPWriteTxWI_Cache(struct rtmp_adapter *pAd, TXWI_STRUC *pTxWI, TX_BLK *pT
 #ifdef MCS_LUT_SUPPORT
 	lut_enable = false;
 	if (RTMP_TEST_MORE_FLAG(pAd, fASIC_CAP_MCS_LUT) &&
-		(pTxBlk->Wcid < 128) &&
-		(pMacEntry && pMacEntry->bAutoTxRateSwitch == true))
-	{
+	    (pTxBlk->Wcid < 128) &&
+	    (pMacEntry && pMacEntry->bAutoTxRateSwitch == true)) {
 		HTTRANSMIT_SETTING rate_ctrl;
 
 		rate_ctrl.field.MODE = phy_mode;
@@ -692,11 +667,9 @@ INT rtmp_mac_set_band(struct rtmp_adapter *pAd, int  band)
 {
 	uint32_t val, band_cfg;
 
-
 	band_cfg = mt7612u_read32(pAd, TX_BAND_CFG);
 	val = band_cfg & (~0x6);
-	switch (band)
-	{
+	switch (band) {
 		case BAND_5G:
 			val |= 0x02;
 			break;
@@ -719,8 +692,7 @@ void mt7612u_mac_set_ctrlch(struct rtmp_adapter *pAd, u8 extch)
 
 	band_cfg = mt7612u_read32(pAd, TX_BAND_CFG);
 	val = band_cfg & (~0x1);
-	switch (extch)
-	{
+	switch (extch) {
 		case EXTCHA_ABOVE:
 			val &= (~0x1);
 			break;
@@ -768,16 +740,13 @@ VOID ReSyncBeaconTime(struct rtmp_adapter *pAd)
 		The updated BeaconInterval Value will affect Beacon Interval after two TBTT
 		beacasue the original BeaconInterval had been loaded into next TBTT_TIMER
 	*/
-	if (Offset == (BCN_TBTT_OFFSET-2))
-	{
+	if (Offset == (BCN_TBTT_OFFSET-2)) {
 		csr.word = mt7612u_read32(pAd, BCN_TIME_CFG);
 
 		/* ASIC register in units of 1/16 TU = 64us*/
 		csr.field.BeaconInterval = (pAd->CommonCfg.BeaconPeriod << 4) - 1 ;
 		mt7612u_write32(pAd, BCN_TIME_CFG, csr.word);
-	}
-	else if (Offset == (BCN_TBTT_OFFSET-1))
-	{
+	} else if (Offset == (BCN_TBTT_OFFSET-1)) {
 		csr.word = mt7612u_read32(pAd, BCN_TIME_CFG);
 		csr.field.BeaconInterval = (pAd->CommonCfg.BeaconPeriod) << 4;
 		mt7612u_write32(pAd, BCN_TIME_CFG, csr.word);
@@ -836,8 +805,7 @@ INT rtmp_mac_pbf_init(struct rtmp_adapter *pAd)
 	pbf_regs = &rlt_pbf_regs[0];
 	tb_size = (sizeof(rlt_pbf_regs) / sizeof(struct rtmp_reg_pair));
 
-	if ((pbf_regs != NULL) && (tb_size > 0))
-	{
+	if ((pbf_regs != NULL) && (tb_size > 0)) 	{
 		for (idx = 0; idx < tb_size; idx++) {
 			mt7612u_write32(pAd, pbf_regs->Register, pbf_regs->Value);
 			pbf_regs++;
@@ -851,66 +819,66 @@ INT rtmp_mac_pbf_init(struct rtmp_adapter *pAd)
 	ASIC register initialization sets
 */
 struct rtmp_reg_pair MACRegTable[] = {
-	{LEGACY_BASIC_RATE,		0x0000013f}, /*  Basic rate set bitmap*/
-	{HT_BASIC_RATE,		0x00008003}, /* Basic HT rate set , 20M, MCS=3, MM. Format is the same as in TXWI.*/
-	{MAC_SYS_CTRL,		0x00}, /* 0x1004, , default Disable RX*/
-	{RX_FILTR_CFG,		0x17f97}, /*0x1400  , RX filter control,  */
-	{BKOFF_SLOT_CFG,	0x209}, /* default set short slot time, CC_DELAY_TIME should be 2	 */
-	{TX_SW_CFG1,		0x80606}, /* Gary,2006-08-23 */
-	{TX_LINK_CFG,		0x1020},		/* Gary,2006-08-23 */
-	/*{TX_TIMEOUT_CFG,	0x00182090},	 CCK has some problem. So increase timieout value. 2006-10-09 MArvek RT*/
-	{TX_TIMEOUT_CFG,	0x000a2090},	/* CCK has some problem. So increase timieout value. 2006-10-09 MArvek RT , Modify for 2860E ,2007-08-01*/
+	{ LEGACY_BASIC_RATE,	0x0000013f },	/*  Basic rate set bitmap*/
+	{ HT_BASIC_RATE,	0x00008003 },	/* Basic HT rate set , 20M, MCS=3, MM. Format is the same as in TXWI.*/
+	{ MAC_SYS_CTRL,		0x00 }, 	/* 0x1004, , default Disable RX*/
+	{ RX_FILTR_CFG,		0x17f97 }, 	/*0x1400  , RX filter control,  */
+	{ BKOFF_SLOT_CFG,	0x209 }, 	/* default set short slot time, CC_DELAY_TIME should be 2	 */
+	{ TX_SW_CFG1,		0x80606 }, 	/* Gary,2006-08-23 */
+	{ TX_LINK_CFG,		0x1020 },	/* Gary,2006-08-23 */
+	/*{TX_TIMEOUT_CFG,	0x00182090 },	 CCK has some problem. So increase timieout value. 2006-10-09 MArvek RT*/
+	{ TX_TIMEOUT_CFG,	0x000a2090 },	/* CCK has some problem. So increase timieout value. 2006-10-09 MArvek RT , Modify for 2860E ,2007-08-01*/
 
 	// TODO: shiang-usw, why MT7601 don't need to set this register??
-	{LED_CFG,		0x7f031e46}, /* Gary, 2006-08-23*/
+	{ LED_CFG,		0x7f031e46 }, 	/* Gary, 2006-08-23*/
 
-	/*{TX_RTY_CFG,			0x6bb80408},	 Jan, 2006/11/16*/
+	/*{TX_RTY_CFG,		0x6bb80408},	 Jan, 2006/11/16*/
 /* WMM_ACM_SUPPORT*/
-/*	{TX_RTY_CFG,			0x6bb80101},	 sample*/
-	{TX_RTY_CFG,			0x47d01f0f},	/* Jan, 2006/11/16, Set TxWI->ACK =0 in Probe Rsp Modify for 2860E ,2007-08-03*/
+/*	{TX_RTY_CFG,		0x6bb80101},	 sample*/
+	{ TX_RTY_CFG,		0x47d01f0f},	/* Jan, 2006/11/16, Set TxWI->ACK =0 in Probe Rsp Modify for 2860E ,2007-08-03*/
 
-	{AUTO_RSP_CFG,			0x00000013},	/* Initial Auto_Responder, because QA will turn off Auto-Responder*/
-	{CCK_PROT_CFG,			0x05740003 /*0x01740003*/},	/* Initial Auto_Responder, because QA will turn off Auto-Responder. And RTS threshold is enabled. */
-	{OFDM_PROT_CFG,		0x05740003 /*0x01740003*/},	/* Initial Auto_Responder, because QA will turn off Auto-Responder. And RTS threshold is enabled. */
+	{ AUTO_RSP_CFG,		0x00000013},	/* Initial Auto_Responder, because QA will turn off Auto-Responder*/
+	{ CCK_PROT_CFG,		0x05740003 /* 0x01740003*/ },	/* Initial Auto_Responder, because QA will turn off Auto-Responder. And RTS threshold is enabled. */
+	{ OFDM_PROT_CFG,	0x05740003 /* 0x01740003*/ },	/* Initial Auto_Responder, because QA will turn off Auto-Responder. And RTS threshold is enabled. */
 
-	{MM40_PROT_CFG,		0x3F44084},		/* Initial Auto_Responder, because QA will turn off Auto-Responder*/
+	{ MM40_PROT_CFG,	0x3F44084 },	/* Initial Auto_Responder, because QA will turn off Auto-Responder*/
 	// TODO: shiang-usw, why MT7601 don't need to set this register??
-	{WPDMA_GLO_CFG,		0x00000030},
+	{ WPDMA_GLO_CFG,	0x00000030 },
 
-	{GF20_PROT_CFG,			0x01744004},    /* set 19:18 --> Short NAV for MIMO PS*/
-	{GF40_PROT_CFG,			0x03F44084},
-	{MM20_PROT_CFG,		0x01744004},
+	{ GF20_PROT_CFG,	0x01744004 },    /* set 19:18 --> Short NAV for MIMO PS*/
+	{ GF40_PROT_CFG,	0x03F44084 },
+	{ MM20_PROT_CFG,	0x01744004 },
 
 
-	{TXOP_CTRL_CFG,			0x0000583f, /*0x0000243f*/ /*0x000024bf*/},	/*Extension channel backoff.*/
-	{TX_RTS_CFG,			0x00092b20},
+	{ TXOP_CTRL_CFG,	0x0000583f,	/*0x0000243f*/ /*0x000024bf*/},	/*Extension channel backoff.*/
+	{ TX_RTS_CFG,		0x00092b20 },
 
-	{EXP_ACK_TIME,			0x002400ca},	/* default value */
-	{TXOP_HLDR_ET, 			0x00000002},
+	{ EXP_ACK_TIME,		0x002400ca },	/* default value */
+	{ TXOP_HLDR_ET, 	0x00000002 },
 
 	/* Jerry comments 2008/01/16: we use SIFS = 10us in CCK defaultly, but it seems that 10us
 		is too small for INTEL 2200bg card, so in MBSS mode, the delta time between beacon0
 		and beacon1 is SIFS (10us), so if INTEL 2200bg card connects to BSS0, the ping
 		will always lost. So we change the SIFS of CCK from 10us to 16us. */
-	{XIFS_TIME_CFG,			0x33a41010},
+	{ XIFS_TIME_CFG,	0x33a41010 },
 };
 
 #ifdef CONFIG_AP_SUPPORT
 struct rtmp_reg_pair APMACRegTable[] = {
-	{WMM_AIFSN_CFG,		0x00001173},
-	{WMM_CWMIN_CFG,	0x00002344},
-	{WMM_CWMAX_CFG,	0x000034a6},
-	{WMM_TXOP0_CFG,		0x00100020},
-	{WMM_TXOP1_CFG,		0x002F0038},
-	{TBTT_SYNC_CFG,		0x00012000},
+	{ WMM_AIFSN_CFG,	0x00001173 },
+	{ WMM_CWMIN_CFG,	0x00002344 },
+	{ WMM_CWMAX_CFG,	0x000034a6 },
+	{ WMM_TXOP0_CFG,	0x00100020 },
+	{ WMM_TXOP1_CFG,	0x002F0038 },
+	{ TBTT_SYNC_CFG,	0x00012000 },
 };
 #endif /* CONFIG_AP_SUPPORT */
 
 #ifdef CONFIG_STA_SUPPORT
 struct rtmp_reg_pair STAMACRegTable[] = {
-	{WMM_AIFSN_CFG,	0x00002273},
-	{WMM_CWMIN_CFG,	0x00002344},
-	{WMM_CWMAX_CFG,	0x000034aa},
+	{ WMM_AIFSN_CFG,	0x00002273 },
+	{ WMM_CWMIN_CFG,	0x00002344 },
+	{ WMM_CWMAX_CFG,	0x000034aa },
 };
 #endif /* CONFIG_STA_SUPPORT */
 
@@ -918,29 +886,25 @@ INT rtmp_mac_init(struct rtmp_adapter *pAd)
 {
 	INT idx;
 
-	for (idx = 0; idx < ARRAY_SIZE(MACRegTable); idx++) {
-		mt7612u_write32(pAd, MACRegTable[idx].Register, MACRegTable[idx].Value);
-	}
+	for (idx = 0; idx < ARRAY_SIZE(MACRegTable); idx++)
+		mt7612u_write32(pAd, MACRegTable[idx].Register,
+				MACRegTable[idx].Value);
 
 #ifdef CONFIG_AP_SUPPORT
-	IF_DEV_CONFIG_OPMODE_ON_AP(pAd)
-	{
-		for (idx = 0; idx < ARRAY_SIZE(APMACRegTable); idx++) {
+	IF_DEV_CONFIG_OPMODE_ON_AP(pAd) {
+		for (idx = 0; idx < ARRAY_SIZE(APMACRegTable); idx++)
 			mt7612u_write32(pAd,
 				APMACRegTable[idx].Register,
 				APMACRegTable[idx].Value);
-		}
 	}
 #endif /* CONFIG_AP_SUPPORT */
 
 #ifdef CONFIG_STA_SUPPORT
-	IF_DEV_CONFIG_OPMODE_ON_STA(pAd)
-	{
-		for (idx = 0; idx < ARRAY_SIZE(STAMACRegTable); idx++) {
+	IF_DEV_CONFIG_OPMODE_ON_STA(pAd) {
+		for (idx = 0; idx < ARRAY_SIZE(STAMACRegTable); idx++)
 			mt7612u_write32(pAd,
 				STAMACRegTable[idx].Register,
 				STAMACRegTable[idx].Value);
-		}
 	}
 #endif /* CONFIG_STA_SUPPORT */
 
